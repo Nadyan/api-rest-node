d8f189ba51b142c91f9ceb0337f25ea00d637f40
Nadyan <nadyan_sp@hotmail.com>
2021-02-01 19:34:11 -0300

    http responses

--- models/appointments.js
+++ models/appointments.js
@@ -3,20 +3,42 @@ const moment = require('moment');
 
 class Appointment {
     add(appointment, res) {
-        const creationDate = moment().format('YYYY-MM-DD HH:MM:SS');
-        const appointmentDate = moment(appointment.appointmentDate, 'DD/MM/YYYY').format('YYYY-MM-DD HH:MM:SS');
+        const creationDate = moment().format('YYYY-MM-DD HH:mm:ss');
+        const appointmentDate = moment(appointment.appointmentDate, 'DD/MM/YYYY').format('YYYY-MM-DD HH:mm:ss');
         
-        const appointmentWithDate = {...appointment, creationDate, appointmentDate};
-        
-        const sql = `INSERT INTO Appointments SET ?`;
-        
-        connection.query(sql, appointmentWithDate, (err, result) => {
-            if (err) {
-                res.json(err);
-            } else {
-                res.json(result);
+        const validDate = moment(appointmentDate).isSameOrAfter(creationDate);
+        const validClient = appointment.client.length >= 3;
+
+        const validations = [
+            {
+                name: 'date',
+                msg: 'Appointment Date must be after the actual date.',
+                valid: validDate
+            },
+            {
+                name: 'client',
+                msg: 'Client name must have at least 3 chars.',
+                valid: validClient
             }
-        });
+        ];
+
+        const errors = validations.filter(field => !field.valid);
+
+        if (errors.length) {
+            res.status(400).json(errors);
+        } else {
+            const appointmentWithDate = {...appointment, creationDate, appointmentDate};
+        
+            const sql = `INSERT INTO Appointments SET ?`;
+            
+            connection.query(sql, appointmentWithDate, (err, result) => {
+                if (err) {
+                    res.status(400).json(err);
+                } else {
+                    res.status(201).json(result.insertId);
+                }
+            });
+        }        
     }
 }
 
