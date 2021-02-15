a9071907c0ccaf236097f8f498be364d3fa8c766
Nadyan <nadyan_sp@hotmail.com>
2021-02-15 11:08:46 -0300

    patch and delete

--- controllers/appointments.js
+++ controllers/appointments.js
@@ -2,12 +2,27 @@ const Appointment = require('../models/appointments');
 
 module.exports = app => {
     app.get('/appointments', (req, res) => {
-        res.send('Appointments route!');
+        Appointment.index(res);
+    });
+
+    app.get('/appointments/:id', (req, res) => {
+        const id = parseInt(req.params.id);
+        Appointment.index_id(id, res);
     });
 
     app.post('/appointments', (req, res) => {
         const appointment = req.body;
         Appointment.add(appointment, res);
-        
+    });
+
+    app.patch('/appointments/:id', (req, res) => {
+        const id = parseInt(req.params.id);
+        const values = req.body;
+        Appointment.edit(id, values, res);
+    });
+
+    app.delete('/appointments/:id', (req, res) => {
+        const id = parseInt(req.params.id);
+        Appointment.delete(id, res);
     });
 }
--- models/appointments.js
+++ models/appointments.js
@@ -2,6 +2,30 @@ const connection = require('../database/connection');
 const moment = require('moment');
 
 class Appointment {
+    index(res) {
+        const sql = 'SELECT * FROM Appointments';
+
+        connection.query(sql, (err, result) => {
+            if (err) {
+                res.status(400).json(err);
+            } else {
+                res.status(200).json(result);
+            }
+        });
+    }
+
+    index_id(id, res) {
+        const sql = 'SELECT * FROM Appointments WHERE id=?';
+
+        connection.query(sql, id, (err, result) => {
+            if (err) {
+                res.status(400).json(err);
+            } else {
+                res.status(200).json(result);
+            }
+        })
+    }
+
     add(appointment, res) {
         const creationDate = moment().format('YYYY-MM-DD HH:mm:ss');
         const appointmentDate = moment(appointment.appointmentDate, 'DD/MM/YYYY').format('YYYY-MM-DD HH:mm:ss');
@@ -35,10 +59,39 @@ class Appointment {
                 if (err) {
                     res.status(400).json(err);
                 } else {
-                    res.status(201).json(result.insertId);
+                    const insertId = result.insertId;
+                    res.status(201).json({...appointmentWithDate, insertId});
                 }
             });
         }        
+    }
+
+    edit(id, values, res) {
+        if (values.appointmentDate) {
+            values.appointmentDate = moment(values.appointmentDate, 'DD/MM/YYYY').format('YYYY-MM-DD HH:mm:ss');
+        }
+
+        const sql = 'UPDATE Appointments SET ? WHERE id = ?';
+
+        connection.query(sql, [values, id], (err, result) => {
+            if (err) {
+                res.status(400).json(err);
+            } else {
+                res.status(200).json({...values, id});
+            }
+        });
+    }
+
+    delete(id, res) {
+        const sql = 'DELETE FROM Appointments WHERE id = ?';
+
+        connection.query(sql, id, (err, result) => {
+            if (err) {
+                res.status(400).json(err);
+            } else {
+                res.status(200).json({id});
+            }
+        });
     }
 }
 
